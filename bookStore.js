6e57f65dd7e0ceef59b25573ba82fe550196a6a4
tasnimchy96 <tasnim.achy96@gmail.com>
2023-04-25 16:50:32 +0600

    Fix style with the wireframe

--- bookStore.js
+++ bookStore.js
@@ -1,68 +1,68 @@
-import Book from "./book.js";
+import Book from './book.js';
+
 class BookCollection {
   constructor() {
-    this.books = JSON.parse(localStorage.getItem("books")) || [];
-    this.bookList = document.querySelector("#bookList");
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.bookList = document.querySelector('#bookList');
   }
 
   addNewBook(title, author) {
     const newBook = new Book(title, author);
     this.books.push(newBook);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
     this.displayBooks();
   }
 
   removeBook(index) {
     this.books = this.books.filter((book, i) => i !== index);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
     this.displayBooks();
   }
 
   displayBooks() {
-    this.bookList = document.getElementById("bookList");
-    bookList.innerHTML = "";
+    this.bookList = document.getElementById('bookList');
+    this.bookList.innerHTML = '';
 
     this.books.forEach((book, index) => {
-      const li = document.createElement("li");
+      const li = document.createElement('li');
       li.innerHTML = `
         <p>${book.title}</p>
         <p>${book.author}</p>        
       `;
 
-      const removeBtn = document.createElement("button");
-      removeBtn.textContent = "Remove";
-      removeBtn.addEventListener("click", () => {
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent = 'Remove';
+      removeBtn.addEventListener('click', () => {
         this.removeBook(index);
       });
 
       li.appendChild(removeBtn);
-      li.className = "book";
+      li.className = 'book';
       this.bookList.appendChild(li);
     });
   }
 }
 
-const bookForm = document.getElementById("addForm");
+const bookForm = document.getElementById('addForm');
 
 const myCollection = new BookCollection();
 
 function addBook(e) {
   e.preventDefault();
 
-  const titleInput = document.getElementById("title");
-  const authorInput = document.getElementById("author");
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
 
-  if (titleInput.value === "" || authorInput.value === "") {
-    alert("Please enter a title and an author");
+  if (titleInput.value === '' || authorInput.value === '') {
     return;
   }
 
   myCollection.addNewBook(titleInput.value, authorInput.value);
 
-  titleInput.value = "";
-  authorInput.value = "";
+  titleInput.value = '';
+  authorInput.value = '';
 }
 
-bookForm.addEventListener("submit", addBook);
+bookForm.addEventListener('submit', addBook);
 
 myCollection.displayBooks();
